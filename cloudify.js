2b8797c8134cab9672e147a74856d2b2d30955e5
KihtrakRaknas <karthikandsankar@gmail.com>
2020-05-03 23:49:34 -0400

    added minifed file

--- cloudify.js
+++ cloudify.js
@@ -1,20 +1,21 @@
-let socketScript = document.createElement('script')
-socketScript.src = "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.slim.js"
-socketScript.onload = function () {
+//Generate minified code: uglifyjs --compress --mangle --output couldify.min.js --toplevel --warn -- cloudify.js
+let socketScript = document.createElement('script');
+socketScript.src = "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.slim.js";
+socketScript.onload = () => {
         //console.log("script added!")
         let socket = io("https://cloud-variable.herokuapp.com/");//http://cloudify.kihtrak.com
-        socket.on('init', function(obj){
+        socket.on('init', (obj)=>{
             //console.log(obj)
             for(let prop in obj){
-                oldGlobal[prop] = obj[prop]
-                window[prop] = obj[prop]
+                oldGlobal[prop] = obj[prop];
+                window[prop] = obj[prop];
             }
         });  
-        socket.on('newVal', function(newObj){
+        socket.on('newVal', (newObj)=>{
             //console.log("new val: "+newObj)
-            let prop = Object.keys(newObj)[0]
-            oldGlobal[prop] = newObj[prop]
-            window[prop] = newObj[prop]
+            let prop = Object.keys(newObj)[0];
+            oldGlobal[prop] = newObj[prop];
+            window[prop] = newObj[prop];
         });
         let oldGlobal = {}
         setInterval(()=>{
@@ -22,11 +23,11 @@ socketScript.onload = function () {
                 if (prop.indexOf("cloud_") == 0){
                         if(oldGlobal[prop] !== window[prop]){
                             //console.log(prop + " has changed from "+oldGlobal[prop]+" to "+window[prop])
-                            oldGlobal[prop] = window[prop]
+                            oldGlobal[prop] = window[prop];
                             socket.emit('varChanged', {[prop]: window[prop]});
                         }
                 }
             }
         },500)  
  }
- document.head.appendChild(socketScript)
+ document.head.appendChild(socketScript);
