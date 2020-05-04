57e266f191190e157fb469a8b2425f610292cf40
KihtrakRaknas <karthikandsankar@gmail.com>
2020-05-04 00:15:35 -0400

    fixed minified issues

--- cloudify.js
+++ cloudify.js
@@ -1,6 +1,6 @@
-//Generate minified code: uglifyjs --compress --mangle --output couldify-min.js --toplevel --warn -- cloudify.js
-let socketScript = document.createElement('script');
-socketScript.src = "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.slim.js";
+//Generate minified code: uglifyjs --compress --mangle --output cloudify.min.js --toplevel --warn -- cloudify.js
+let socketScript = document.createElement('script')
+socketScript.src = "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.slim.js"
 socketScript.onload = () => {
         //console.log("script added!")
         let socket = io("http://cloudify.kihtrak.com");
