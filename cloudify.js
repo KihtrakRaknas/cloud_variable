97730d2837fe0ee535b5156ed5eab9f336b3aba8
Kihtrak Raknas <karthikandsankar@gmail.com>
2022-11-03 12:06:48 -0400

    formatted cloudify.js

--- cloudify.js
+++ cloudify.js
@@ -1,47 +1,47 @@
-//Generate minified code: uglifyjs --compress --mangle --output cloudify.min.js --toplevel --warn -- cloudify.js
+// Generate minified code: uglifyjs --compress --mangle --output cloudify.min.js --toplevel --warn -- cloudify.js
 let socketScript = document.createElement('script')
 socketScript.src = "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.slim.js"
 socketScript.onload = () => {
-        //console.log("script added!")
-        let socket = io("https://cloud-variable.herokuapp.com/");//http://cloudify.kihtrak.com
-        let URL = encodeURIComponent(window.location.protocol + "//" + window.location.hostname).replace(/\./g, '%2E')
-        socket.on('init-'+URL, (obj)=>{
-            //console.log(obj)
-            for(let prop in obj){
-                oldGlobal[prop] = obj[prop];
-                if(window[prop] == null)
-                    window[prop] = obj[prop];
-            }
-        });  
-        socket.on('newVal-'+URL, (newObj)=>{
-            //console.log("new val: "+newObj)
-            let prop = Object.keys(newObj)[0];
-            window[prop] = newObj[prop];
-            oldGlobal[prop] = JSON.parse(JSON.stringify(newObj[prop]));
-        });
-        let oldGlobal = {}
-        let clean=(variable)=>{
-            //console.log(variable)
-            if(typeof variable == "number" || typeof variable == "string")
-                return variable
-            if(typeof variable == "object")
-                return JSON.stringify(variable)
-            if(typeof variable == "function")
-                return variable.toString()
-            return variable
+    // console.log("script added!")
+    let socket = io("https://cloud-variable.herokuapp.com/");//http://cloudify.kihtrak.com
+    let URL = encodeURIComponent(window.location.protocol + "//" + window.location.hostname).replace(/\./g, '%2E')
+    socket.on('init-' + URL, (obj) => {
+        //console.log(obj)
+        for (let prop in obj) {
+            oldGlobal[prop] = obj[prop];
+            if (window[prop] == null)
+                window[prop] = obj[prop];
         }
-        setInterval(()=>{
-            for (let prop in window) { //global[prop]
-                if (prop.indexOf("cloud_") == 0){
-                        //console.log(clean(oldGlobal[prop]) +" !== "+clean(window[prop]))
-                        if(clean(oldGlobal[prop]) !== clean(window[prop])){
-                            //console.log(prop + " has changed from "+oldGlobal[prop]+" to "+window[prop])
-                            oldGlobal[prop] = (typeof window[prop] == "object")?JSON.parse(JSON.stringify(window[prop])):window[prop];                                
-                            //console.log({[prop]: window[prop]})
-                            socket.emit('varChanged', {[prop]: (typeof window[prop] == "function")?window[prop].toString():window[prop]});
-                        }
+    });
+    socket.on('newVal-' + URL, (newObj) => {
+        // console.log("new val: "+newObj)
+        let prop = Object.keys(newObj)[0];
+        window[prop] = newObj[prop];
+        oldGlobal[prop] = JSON.parse(JSON.stringify(newObj[prop]));
+    });
+    let oldGlobal = {}
+    let clean = (variable) => {
+        // console.log(variable)
+        if (typeof variable == "number" || typeof variable == "string")
+            return variable
+        if (typeof variable == "object")
+            return JSON.stringify(variable)
+        if (typeof variable == "function")
+            return variable.toString()
+        return variable
+    }
+    setInterval(() => {
+        for (let prop in window) { //global[prop]
+            if (prop.indexOf("cloud_") == 0) {
+                // console.log(clean(oldGlobal[prop]) +" !== "+clean(window[prop]))
+                if (clean(oldGlobal[prop]) !== clean(window[prop])) {
+                    // console.log(prop + " has changed from "+oldGlobal[prop]+" to "+window[prop])
+                    oldGlobal[prop] = (typeof window[prop] == "object") ? JSON.parse(JSON.stringify(window[prop])) : window[prop];
+                    // console.log({[prop]: window[prop]})
+                    socket.emit('varChanged', { [prop]: (typeof window[prop] == "function") ? window[prop].toString() : window[prop] });
                 }
             }
-        },500)
- }
- document.head.appendChild(socketScript);
+        }
+    }, 500)
+}
+document.head.appendChild(socketScript);
